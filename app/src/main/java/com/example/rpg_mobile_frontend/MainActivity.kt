a29f39a3bac35fa18e42fe0d55d0a97d73d390b4
Luís Henrique dos Santos <144742302+LuisH019@users.noreply.github.com>
2025-09-29 10:12:12 -0300

    Primeiro commit do projeto de frontend

---
+++ app/src/main/java/com/example/rpg_mobile_frontend/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.rpg_mobile_frontend
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.rpg_mobile_frontend.ui.theme.Rpg_mobile_frontendTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Rpg_mobile_frontendTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Rpg_mobile_frontendTheme {
+        Greeting("Android")
+    }
+}
